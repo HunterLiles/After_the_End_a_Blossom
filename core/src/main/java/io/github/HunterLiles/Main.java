252c0ffbdb679f9b8d7b048d5b046f77957adc5e
hunter <ghosthunterliles@gmail.com>
2025-05-31 15:07:25 -0500

    Removed everything I didn't like so that I could start from almost scratch.

--- core/src/main/java/io/github/HunterLiles/Main.java
+++ core/src/main/java/io/github/HunterLiles/Main.java
@@ -1,35 +1,18 @@
 package io.github.HunterLiles;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import io.github.HunterLiles.screens.*;
-import io.github.HunterLiles.screens.MedievalScene;
 
 public class Main extends Game {
-    SpriteBatch batch;
 
     // Runs when the game starts
     @Override
     public void create() {
-        SceneManager sceneManager = SceneManager.getInstance();
-        sceneManager.initialize(this);
 
-        // Add our game screens to the SceneManager
-        sceneManager.registerScene("stone_age", new StoneAgeScene(this));
-        sceneManager.registerScene("ancient_age", new AncientScene(this));
-        sceneManager.registerScene("medieval_age", new MedievalScene(this));
-
-        // Start the game with the Stone Age screen
-        sceneManager.changeScene("stone_age");
     }
 
     // Runs when the game closes
     @Override
     public void dispose() {
         super.dispose();
-
-        // Clean up the SceneManager to prevent memory leaks
-        SceneManager.getInstance().dispose();
     }
 }
