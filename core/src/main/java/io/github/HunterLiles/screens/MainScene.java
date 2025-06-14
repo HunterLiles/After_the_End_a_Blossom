7abaf17d1e97779f3506c5a2f1266b38b42a160d
hunter <ghosthunterliles@gmail.com>
2025-06-13 21:15:57 -0500

    Did some stuff

--- core/src/main/java/io/github/HunterLiles/screens/MainScene.java
+++ core/src/main/java/io/github/HunterLiles/screens/MainScene.java
@@ -1,5 +1,14 @@
 package io.github.HunterLiles.screens;
 
-public class MainScene {
+import com.badlogic.gdx.Screen;
 
+public class MainScene implements Screen {
+
+    @Override public void resize(int width, int height) { }
+    @Override public void render(float delta) { }
+    @Override public void pause() { }
+    @Override public void resume() { }
+    @Override public void hide() { }
+    @Override public void dispose() { }
+    @Override public void show() { }
 }
